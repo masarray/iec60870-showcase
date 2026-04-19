6fe7258a22d0a4d44bb553e584123258902085df
masarray <ari.sulistiono@gmail.com>
2026-04-19 07:24:07 +0700

    Hardcode Astro base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig(({ command }) => {
-  const isDev = command === "dev";
-
-  return {
-    site: "https://masarray.github.io",
-    base: isDev ? "/" : "/iec60870-showcase/",
-    output: "static",
-  };
+export default defineConfig({
+  site: "https://masarray.github.io",
+  base: "/iec60870-showcase/", // ← WAJIB
+  trailingSlash: "ignore",
+  build: { assets: "_astro" },
 });
